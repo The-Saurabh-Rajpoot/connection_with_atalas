3ac80a660e2d59cb97045c4581041d05290b5048
saurabh rajpoot <saurabh941193@gmail.com>
2023-03-28 18:03:26 +0530

    updated post schema with user property and updated controller for post with token varification

--- controlers/controlers.post.js
+++ controlers/controlers.post.js
@@ -1,10 +1,19 @@
 const posts=require("../models/post.js");
+const secreteKey="itisMykey";
+const jwt=require('jsonwebtoken')
 const mongoose=require("mongoose")
 const postControler= (req,res)=>{
+    const payload=jwt.verify(req.headers.token,secreteKey);
+    if(!payload){
+        res.status(404).send("you are not autharized")
+        return;
+    }
+   
     const post=new posts({
     titel:req.body.titel,
     body:req.body.body,
-    image:req.body.image
+    image:req.body.image,
+    user:payload.username
 })
 post.save().then(data=>{res.status(200).send(data)}).catch(err=>res.status(500).send(err));
 
