223a39f949e1f34cb30fd668a21a82a408ba1b51
saurabh rajpoot <saurabh941193@gmail.com>
2023-03-28 12:46:44 +0530

    mongodb connection file added

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
-const mongoUrl="mongodb://localhost:27017/newSigninSignupdatabase";
-const mongoose=require("mongoose");
-const mongoconnetion=mongoose.connect(mongoUrl,{ useNewUrlParser: true }).then(console.log("connectd to database")).catch((err)=>{console.log(err)})
-module.exports=mongoconnetion;
+const mongoUrl = "mongodb://localhost:27017/newSigninSignupdatabase";
+const mongoose = require("mongoose");
+
+const mongoConnection = () => {
+  mongoose.connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.log(`Error connecting to database: ${err}`));
+};
