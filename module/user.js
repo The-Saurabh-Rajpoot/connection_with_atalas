d86dba69be5da92223dfc9278614a330b21a5e7f
saurabh rajpoot <saurabh941193@gmail.com>
2023-03-28 07:01:34 +0530

    New method for password encryption created on userSchema

--- module/user.js
+++ module/user.js
@@ -14,3 +14,14 @@ const userSchema= new mongoose.Schema(
     }
 }
 );
+userSchema.pre('save',async function(next){
+    const user=this;
+    if(!user.isModified('password')){
+        return next();
+    }
+    const salt= await bcrypt.genSalt(10);
+    const hasedPassword= await bcrypt.hash(user.password,salt);
+    user.password=hasedPassword;
+    next();
+})
+module.exports=mongoose.model("user",userSchema);
