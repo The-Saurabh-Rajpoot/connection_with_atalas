b4f0b77e30615394788d0fd2314610f7d47b2b88
saurabh rajpoot <saurabh941193@gmail.com>
2023-03-28 09:50:59 +0530

    user controler added on schema

---
+++ controlers/controlers.user.js
@@ -0,0 +1,15 @@
+const user=require("../models/user.js");
+
+const newUser= new user({
+    username:req.body.username,
+    email:req.body.email,
+    password:req.body.password
+});
+newUser.save().then(data=>{
+    
+        res.status(200).send(data);
+})
+.catch(err=>{
+    res.status(500).send("some error occourd")
+
+})
