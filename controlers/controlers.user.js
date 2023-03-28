0e190f92ddef5c981a441ea4bd72bda9394cfbb9
saurabh rajpoot <saurabh941193@gmail.com>
2023-03-28 17:35:54 +0530

    added login controller in user.controler.js

--- controlers/controlers.user.js
+++ controlers/controlers.user.js
@@ -1,5 +1,8 @@
 const user=require("../models/user.js");
-const userControler=(req,res)=>{
+const bcrypt=require("bcrypt")
+const secreteKey="itisMykey"
+const jwt=require("jsonwebtoken")
+const userRegister=(req,res)=>{
     const newUser= new user({
     username:req.body.username,
     email:req.body.email,
@@ -13,4 +16,23 @@ newUser.save().then(data=>{
     res.status(500).send("some error occourd")
 
 })}
-module.exports=userControler;
+const userLogin=async (req,res)=>{
+    const password=req.body.password;
+    const username=req.body.username;
+   const userData= await user.findOne({username:username});
+   console.log(userData);
+   if(!userData){
+    res.status(404).send("user not found");
+    return
+   }
+   const passwordMatched= await bcrypt.compare(password,userData.password);
+   if(!passwordMatched){
+    res.status(500).send("password not matched");
+    return
+   }
+   const token= await jwt.sign(req.body,secreteKey);
+   res.status(200).json({messege:"succfully login",token:`${token}`})
+   
+}
+
+module.exports={userRegister,userLogin};
