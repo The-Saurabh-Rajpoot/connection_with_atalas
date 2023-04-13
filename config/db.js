86167b423b6a3ba0586874f71f3cc44e075a11a4
saurabh rajpoot <saurabh941193@gmail.com>
2023-04-13 20:06:47 +0530

    Post name error rectified

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-const mongoUrl = "mongodb://localhost:27017/newSigninSignupdatabase";
+const mongoUrl = `mongodb+srv://saurabh941193:U0lawKv69vSI6eHO@cluster0.wfhwzur.mongodb.net/InstaData?retryWrites=true&w=majority`
 const mongoose = require("mongoose");
 
 const mongoConnection = () => {
-  mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  mongoose.connect(mongoUrl, { useNewUrlParser: true,useUnifiedTopology:true})
     .then(() => console.log("Connected to database"))
     .catch((err) => console.log(`Error connecting to database: ${err}`));
 };
--- controlers/controlers.post.js
+++ controlers/controlers.post.js
@@ -15,26 +15,26 @@ Post.save().then(data=>{res.status(200).send(data)}).catch(err=>res.status(500).
 }
 const updatePost= async (req,res)=>{
     const Postid=req.params.id;
-    const payload= await jwt.verify(req.headers.token,secreteKey);
-    const user=payload.username;
-    const postbyid= await post.findById(Postid);
-    
-    if(postbyid.user!=user){
-        res.status(401).send("can not edit other's post");
-        return;
-        }
-    post.findByIdAndUpdate(Postid,req.body,{new:true}).then((data)=>{
+    const payload=  jwt.verify(req.headers.token,secreteKey);
+    const User=payload.username;
+    // const postbyid= await post.findById(Postid);
+    // console.log(postbyid.user,user,Postid);
+    // if(postbyid.user!=user){
+    //     res.status(401).send("can not edit other's post");
+    //     return;
+    //     }
+     console.log(Postid,User)   
+    Post.findByIdAndUpdate({_id:Postid,user:User},req.body,{new:true}).then((data)=>{
         res.status(200).send(data);
     }).catch(err=>{res.status(404).send(err)})
-
 }
 const findAllPost=async (req,res)=>{
-    await post.find({}).then((data)=>{
+    await Post.find({}).then((data)=>{
         res.status(200).json(data)
     }).catch(err=>{err});
 }
 const deleteByid=(req,res)=>{
-    post.findByIdAndDelete({_id:req.params.id}).then((data)=>{
+    Post.findByIdAndDelete({_id:req.params.id}).then((data)=>{
         res.status(200).send('deleted successfylly')
     }).catch(err=>{err});
 }
